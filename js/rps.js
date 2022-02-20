3b6e9ac9c7d5e153498b470486ce52f4842266d5
dean <dnabusood@gmail.com>
2022-02-19 23:35:17 -0500

    Add event listeners

--- js/rps.js
+++ js/rps.js
@@ -1,73 +1,60 @@
 
-    let num;
     let playerScore = 0;
-    let computerScore = 0;    
+    let computerScore = 0;
+    const SCORE_TO_WIN = 5; 
 
       function computerPlay() {
         const options = ["Rock", "Paper", "Scissors"];
         return options[Math.floor(Math.random() * options.length)];
       }
 
-    function playRound(e, computerSelection) {
-        let playerSelection = e.target;
-        // playerSelection = playerSelection.toUpperCase();
-        // computerSelection = computerSelection.toUpperCase();
+    function playRound(playerSelection, computerSelection) {
+        playerSelection = playerSelection.toUpperCase();
+        computerSelection = computerSelection.toUpperCase();
+
 
         if (playerSelection === "ROCK") {
           if (computerSelection === "ROCK") {
-            num = 0;
-            return "You Tie! You both chose Rock";
+            textField.textContent = "You Tie! You both chose Rock";
           }
           else if(computerSelection === "SCISSORS"){
-            num = 1;
-              return "You Win! Rock beats Scissors";
+            textField.textContent = "You Win! Rock beats Scissors";
           }
           else if(computerSelection === "PAPER"){
-            num = 2;
-              return "You Lose! Paper beats Rock";
+            textField.textContent = "You Lose! Paper beats Rock";
           }
         }
-
         else if (playerSelection === "PAPER") {
           if (computerSelection === "PAPER") {
-            num = 0;
-            return "You Tie! You both chose Paper";
+            textField.textContent = "You Tie! You both chose Paper";
           }
           else if(computerSelection === "ROCK"){
-            num = 1;
-              return "You Win! Paper beats Rock";
+            textField.textContent = "You Win! Paper beats Rock";
           }
           else if(computerSelection === "SCISSORS"){
-            num = 2;
-              return "You Lose! Scissors beats Paper";
+            textField.textContent = "You Lose! Scissors beats Paper";
           }
         }
-
         else if (playerSelection === "SCISSORS") {
           if (computerSelection === "SCISSORS") {
-            num = 0;
-            return "You Tie! You both chose Scissors";
+            textField.textContent = "You Tie! You both chose Scissors";
           }
           else if(computerSelection === "PAPER"){
-            num = 1;
-              return "You Win! Scissors beats Paper";
+            textField.textContent = "You Win! Scissors beats Paper";
           }
           else if(computerSelection === "ROCK"){
-            num = 2;
-              return "You Lose! Rock beats Scissors";
+            textField.textContent = "You Lose! Rock beats Scissors";
           } 
         }
+
     }
 
     function determineWinner(playerScore, computerScore){
-      if(playerScore === computerScore){
-        alert("You tied the computer!");
-      }
-      else if (playerScore > computerScore){
+      if(playerScore === SCORE_TO_WIN){
         alert("You beat the computer!");
-      } 
-      else{
-        alert("You lost to the computer!");
+      }
+      else if(computerScore === SCORE_TO_WIN){
+        alert("The computer beat you!");
       }
     }
 
@@ -77,34 +64,13 @@
     buttons.forEach(button => {
         button.addEventListener("click", (e) => {
 
-            console.log(e.target);
+            playRound(button.id, computerPlay());
         });
     });
 
+    const textField = document.createElement("h3");
+    textField.textContent = "Make your selection!";
+    textField.setAttribute("style", "text-align: center");
 
-    // function game(){
-    //   let playerScore = 0;
-    //   let computerScore = 0;
-
-    //     for(let i = 0; i < 5; i++){
-    //         playerSelection = prompt("Rock, Paper, or Scissors?");
-    //         alert( playRound(playerSelection, computerPlay()));
-
-    //         if(num === 1){
-    //           playerScore++;
-    //         }
-    //         else if(num === 2){
-    //           computerScore++;
-    //         }
-
-    //         let gamesPlayed = (i + 1); //starts at 1
-    //         alert("Total games played: "+(gamesPlayed)+"\n\nPlayer wins: "+(playerScore)+
-    //         "\nComputer wins: "+(computerScore));
-
-    //         if(gamesPlayed === 5){
-    //           determineWinner(playerScore, computerScore);
-    //         }
-    //     } 
-    // }
-    
-    // game();
+    const mainDiv = document.querySelector("div");
+    mainDiv.appendChild(textField);
